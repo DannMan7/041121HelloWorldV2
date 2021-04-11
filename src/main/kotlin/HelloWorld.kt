445942cb0acfdeef78a2c92cbe7873b591b4cd19
BuildTools <unconfigured@null.spigotmc.org>
2021-04-11 16:01:06 -0400

    Testing github connection

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,4 +1,30 @@
+typealias EmployeeSet = Set<Employee>
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello World")
+    // Declarations
+    var number:Int
+    number = 10;
+    number = 20
+
+
+
+    val names = arrayListOf("John","Jane", "Mary")
+    println(names[1])
+
+    val employees: EmployeeSet
+
+    val employee1 = Employee("Jon Doe", 500)
+    employee1.name = "John Jones"
+    val employee2: Employee
+    val number2 = 100
+
+    if(number < number2) {
+        employee2 = Employee("Jane Smith", 400)
+    }
+    else {
+        employee2 = Employee("Mike Watson", 150)
+    }
+
 }
+class Employee(var name: String, val id: Int)
